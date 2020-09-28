dfac4dcb1fda64adc5e4c4d57d74c88aaa87eab6
Vinícius Viterbo <viniciusvviterbo@yahoo.com.br>
2020-09-27 21:14:17 -0300

    Implemented pattern reading from file

--- mlp.cpp
+++ mlp.cpp
@@ -11,56 +11,6 @@ using namespace std;
 
 //Miscelaneous usefull functions and procedures
 #pragma region Util
-// Converts a list of tuples to a matrix used as a pattern for the entries
-double **listToPatternMatrix(list<double *> listToConvert, int rowCount, int columnCount)
-{
-    list<double *>::iterator it;
-    int i, j;
-    double **pattern = (double **)calloc(rowCount, sizeof(double *));
-    for (i = 0, it = listToConvert.begin(); i < rowCount; i++, it++)
-    {
-        pattern[i] = (double *)calloc(columnCount + 1, sizeof(double));
-        double *row = *it;
-        for (j = 0; j < columnCount; j++)
-        {
-            pattern[i][j] = row[j];
-        }
-        pattern[i][columnCount] = 1;
-    }
-    return pattern;
-}
-
-// Reads from the pattern file and returns a parsed list with the entries
-double **readPatternFile(int inputLength)
-{
-    list<double *> pattern;                       // Initiates the entries/input pattern
-    double *buffer;                               // Initiates the buffer to inform the input matriz values
-    char *tmpLine;                                // Initiates the buffer for reading the file lines
-    FILE *johnLennon = fopen(PATTERN_FILE, "rb"); // Instantiates the file pointer
-
-    if (johnLennon == NULL) // Tests readability of file
-        cerr << "Error opening file";
-    else // If file is readable, excutes
-    {
-        char *line = NULL;                               // Declares the line for each iteration
-        size_t len = 0;                                  // Declares the size of the line for each iteration
-        while ((getline(&line, &len, johnLennon)) != -1) // While the file has lines to be read, executes
-        {
-            buffer = (double *)calloc(inputLength, sizeof(double)); // Clears the buffer for each iteration
-
-            for (int i = 0; i < inputLength; i++) // Reads the relevant values (for now) of the line
-            {
-                tmpLine = strtok(line, " "); // Splits the line by spaces
-                buffer[i] = atof(tmpLine);   // Converts the read value to float/double
-            }
-
-            pattern.push_back(buffer); // Appends the values to the buffer
-        }
-    }
-    fclose(johnLennon);
-    return listToPatternMatrix(pattern, pattern.size(), inputLength);
-}
-
 // Multiply a matrix by a vector and return the result
 double *matrixVectorMultiplication(double **matrix, double *vec, int rowCountMatrix, int columnCountMatrix, int rowCountVector)
 {
@@ -69,9 +19,9 @@ double *matrixVectorMultiplication(double **matrix, double *vec, int rowCountMat
 
     if (columnCountMatrix == rowCountVector + 1)
     {
-        for (int i = 0; i < rowCountMatrix; i++) 
+        for (int i = 0; i < rowCountMatrix; i++)
         {
-            for (int j = 0; j < rowCountVector; j++) 
+            for (int j = 0; j < rowCountVector; j++)
             {
                 result[i] += matrix[i][j] * vec[j];
             }
@@ -117,7 +67,6 @@ double **vectorMultiplication(double *a, double *b, int lengthA, int lengthB)
     }
     return result;
 }
-
 #pragma endregion
 
 // Representation of the values with a given input at each step in the forward motion
@@ -236,7 +185,7 @@ public:
     {
         // Finds the net value (sum of weighted inputs) received by the neuron in the hidden layer
         double *netHiddenLayer = matrixVectorMultiplication(this->hiddenLayer, input, this->hiddenLayerLength, this->inputLength + 1, this->inputLength);
-        
+
         // Finds the values calculated by the hidden layer
         this->activation(netHiddenLayer, this->hiddenLayerLength);
 
@@ -244,7 +193,7 @@ public:
 
         // Finds the net value (sum of weighted inputs) received by the neuron in the output layer
         double *netOutputLayer = matrixVectorMultiplication(this->outputLayer, netHiddenLayer, this->outputLayerLength, this->hiddenLayerLength + 1, this->hiddenLayerLength);
-        
+
         // Finds the values calculated by the hidden layer
         this->activation(netOutputLayer, this->outputLayerLength);
 
@@ -261,9 +210,9 @@ public:
     {
 
         int count = 0;
-        //vectors used in the method
+        // Vectors used in the method
         double *Xp, *Yp, *errors, *dNetOutput, *deltaOutput, *dNetHidden, *deltaHidden, *outputDerivative;
-        //matrices used in the method
+        // Matrices used in the method
         double **outputLayerCorrection, **hiddenLayerCorrection;
         State results;
 
@@ -286,15 +235,12 @@ public:
 #pragma region Output layer manipulation
                 // Declares a vector for the calculated errors
                 errors = (double *)calloc(this->outputLayerLength, sizeof(double));
-                //cout << "Errors: [";
                 // Calculates the error for each value obtained
                 for (int i = 0; i < this->outputLayerLength; i++)
                 {
                     errors[i] = Yp[i] - results.fNetOutput[i];
                     squaredError += pow(errors[i], 2);
-                    //cout << errors[i] << " ";
                 }
-                //cout << "]\n";
 
                 // Finds the derivative of the line that represents the error
                 dNetOutput = dNet(results.fNetOutput, this->outputLayerLength);
@@ -309,14 +255,14 @@ public:
 #pragma endregion
 
 #pragma region Hidden layer manipulation
-                outputDerivative = vectorMatrixMultiplication(deltaOutput, this->outputLayer, this->outputLayerLength, this->outputLayerLength , this->hiddenLayerLength);
-                
+                outputDerivative = vectorMatrixMultiplication(deltaOutput, this->outputLayer, this->outputLayerLength, this->outputLayerLength, this->hiddenLayerLength);
+
                 // Declares a vector for the calculated derivatives
                 deltaHidden = (double *)calloc(this->hiddenLayerLength, sizeof(double));
 
                 // Finds the derivative of the line that represents the error
                 dNetHidden = dNet(results.fNetHidden, this->hiddenLayerLength);
-               
+
                 // Calculates the derivative for each error stored
                 for (int i = 0; i < this->hiddenLayerLength; i++)
                 {
@@ -326,7 +272,7 @@ public:
 
 #pragma region Effective training
                 outputLayerCorrection = vectorMultiplication(deltaOutput, results.fNetHidden, this->outputLayerLength, this->hiddenLayerLength);
-    
+
                 for (int i = 0; i < this->outputLayerLength; i++)
                 {
                     for (int j = 0; j < this->hiddenLayerLength + 1; j++)
@@ -348,7 +294,8 @@ public:
 
             squaredError /= datasetLength;
             count++;
-            //free all used memory
+
+            // Clear all used memory
             free(errors);
             free(dNetOutput);
             free(deltaOutput);
@@ -356,67 +303,63 @@ public:
             free(deltaHidden);
             free(outputDerivative);
         }
-        
+
         cout << "Number of epochs = " << count << endl;
     }
 
 #pragma endregion
 };
 
-int main()
+int main(int argc, char *argv[])
 {
-    // Declares the MLP class
-    MLP *mlp = new MLP(2, 2, 1);
+    // Declares and reads the main values of the dataset input
+    int datasetLength, inputLength, outputLength, hiddenLength = atoi(argv[1]);
+    double trainingRate = atof(argv[2]), threshold = atof(argv[3]);
 
-    double **input = (double **)calloc(4, sizeof(double *));
-    double **output = (double **)calloc(4, sizeof(double *));
-    for (int i = 0; i < 4; i++)
+    cin >> datasetLength >> inputLength >> outputLength;
+
+    // Alocates memory for the dataset reading
+    double **input = (double **)calloc(datasetLength, sizeof(double *));
+    double **output = (double **)calloc(datasetLength, sizeof(double *));
+    
+    // Reads the dataset data
+    for (int i = 0; i < datasetLength; i++)
     {
-        input[i] = (double *)calloc(3, sizeof(double));
-        output[i] = (double *)calloc(1, sizeof(double));
+        input[i] = (double *)calloc(inputLength, sizeof(double));
+        output[i] = (double *)calloc(outputLength, sizeof(double));
+
+        for (int j = 0; j < inputLength; j++)
+        {
+            cin >> input[i][j];
+        }
+
+        for (int j = 0; j < outputLength; j++)
+        {
+            cin >> output[i][j];
+        }
     }
 
-    input[0][0] = 0;
-    input[0][1] = 0;
-    input[1][0] = 0;
-    input[1][1] = 1;
-    input[2][0] = 1;
-    input[2][1] = 0;
-    input[3][0] = 1;
-    input[3][1] = 1;
-    output[0][0] = 0;
-    output[1][0] = 1;
-    output[2][0] = 1;
-    output[3][0] = 0;
+    // Declares the MLP network class
+    MLP *mlp = new MLP(inputLength, hiddenLength, outputLength);
 
-    mlp->backPropagation(input, output, 4, 0.2, 1e-5);
+    // Executes the neural network training
+    mlp->backPropagation(input, output, datasetLength, trainingRate, threshold);
 
+
+    #pragma region Testing
     State state;
 
-    double *Xp = (double *)calloc(2, sizeof(double));
-    Xp[0] = 0;
-    Xp[1] = 0;
-    state = mlp->forward(Xp);
-
-    cout << "0 XOR 0 = " << round(state.fNetOutput[0]) << '\n';
-
-    Xp[0] = 0;
-    Xp[1] = 1;
-    state = mlp->forward(Xp);
-
-    cout << "0 XOR 1 = " << round(state.fNetOutput[0]) << '\n';
-
-    Xp[0] = 1;
-    Xp[1] = 0;
-    state = mlp->forward(Xp);
-
-    cout << "1 XOR 0 = " << round(state.fNetOutput[0]) << '\n';
-
-    Xp[0] = 1;
-    Xp[1] = 1;
-    state = mlp->forward(Xp);
-
-    cout << "1 XOR 1 = " << round(state.fNetOutput[0]) << '\n';
+    double *Xp;
+    
+    for(int i = 0; i < 3; i++)
+    {
+        Xp = input[i];
+        state = mlp->forward(Xp);
+        cout << "Test " << i << ":" << endl;
+        cout << "\tExpected: " << output[i][0] << endl;
+        cout << "\tObtained: " << round(state.fNetOutput[0]) << '\n';
+    }
+    #pragma endregion
 
     return (0);
 }
